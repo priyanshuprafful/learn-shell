60ac212712a59d0a6e180b81457939724273bbed
priyanshuprafful <priyanshuprafful@gmail.com>
2023-06-22 13:59:27 +0530

    updating ./learn-shell

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,3 +1,5 @@
 DATE=20-06-2023
 echo "Hello , Good Morning , Today the date is ${DATE}"
-echo "This is just for checking "
+# Command Substituion
+DATE=$(date)
+echo "Hello , Good Morning , Today the date is ${DATE}"
