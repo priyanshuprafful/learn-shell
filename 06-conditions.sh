ba4bedf5286c91e92cafe114ebd6c8bea0978b92
priyanshuprafful <priyanshuprafful@gmail.com>
2023-07-02 18:46:49 +0530

    updating ./learn-shell

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -5,7 +5,7 @@ if [ "${number}" -eq 5 ]; then #then can go in next line ,also ; is also fine in
   echo "The number is 5"
 fi
 
-if [ "${string}" == abc]; then
+if [ "${string}" == abc ]; then
   echo "String is abc"
 fi
 
