be2a1947d6e396ac5b3b19dd700613b8f0431900
priyanshuprafful <priyanshuprafful@gmail.com>
2023-07-02 18:36:05 +0530

    updating ./learn-shell

---
+++ 06-conditions.sh
@@ -0,0 +1,12 @@
+ number=$1
+ string=$2
+
+ if [ "${number}" -eq 5 ]; then #then can go in next line ,also ; is also fine in same line
+  echo "The number is 5"
+fi
+
+if [ "${string}" == abc]; then
+  echo "String is abc"
+fi
+
+## It is always a good practice to quote variable in expressions
